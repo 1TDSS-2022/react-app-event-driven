d4cd26252ca273fc3923b761980a920125885e76
RuanSDias <sdias.ruan@gmail.com>
2022-08-26 21:08:59 -0300

    event-driven

---
+++ rm94528/aula05/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MinhasRotas from "./routes/MinhasRotas";
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE EVENTOS EM ELEMENTOS DESESTRUTURADOS</h1>
+            <MinhasRotas />
+        </div>
+    )
+}
