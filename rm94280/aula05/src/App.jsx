3f6d1e21668df1d4560291aa393fe0e3ec93b86a
mouragus <gumora18@hotmail.com>
2022-08-26 21:56:13 -0300

    aula05

---
+++ rm94280/aula05/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import MinhasRotas from "./routes/MinhasRotas"
+
+export default function App() {
+    return(
+        <div>
+            <Menu />
+            <h1>EVENTOS EM ELEMENTOS DESESTRUTURADOS</h1>
+
+            <MinhasRotas />
+        </div>
+    )
+}
---
+++ rm94280/aula05/src/components/menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Menu() {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Novo Valor</Link> </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
