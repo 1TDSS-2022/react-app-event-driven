b180daff25eb9807b10d32afb65337bb660228f8
kellerlivia <rm94275@fiap.com.br>
2022-08-26 21:06:57 -0300

    Event Driven

---
+++ rm94275/aula05/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Menu from "./components/menu/Menu"
+import MinhasRotas from "./routes/MinhasRotas"
+
+export default function App() {
+    return(
+        <div>
+            <Menu />
+            <h1>ESTUDO SOBRE EVENTOS EM ELEMENTOS DESESTRUTURADOS!</h1>
+            
+            <MinhasRotas />
+        </div>
+    )
+}
